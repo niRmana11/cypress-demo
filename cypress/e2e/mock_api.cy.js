76b323d909994d92ba981c873c573a052b12e0cd
niRmana11 <nirmanaherath123@gmail.com>
2026-03-14 09:38:42 +0530

    update mocking testing

--- cypress/e2e/mock_api.cy.js
+++ cypress/e2e/mock_api.cy.js
@@ -2,10 +2,17 @@ describe("Mock API Example", () => {
   it("mock API response", () => {
     cy.intercept("GET", "/api/user", {
       statusCode: 200,
-
-      body: {
-        name: "Mock User",
-      },
+      body: { name: "Mock User" },
     }).as("getUser");
+
+    cy.visit("http://localhost:3000");
+
+    cy.window().then((win) => {
+      win.fetch("/api/user");
+    });
+
+    cy.wait("@getUser").then((interception) => {
+      expect(interception.response.body.name).to.equal("Mock User");
+    });
   });
 });
